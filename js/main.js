c1dcaa29e2af0a8b0f0471731150a9425599f9b0
Jonathan Cohen <jonathancohen@Jonathans-MacBook-Pro.local>
2017-03-10 12:16:13 -0500

    created functions for game logic

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ var cards = ["queen","queen","king","king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped Queen");
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped King");
-
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+var checkForMatch = function() {
+	if(cardsInPlay[0]===cardsInPlay[1]) {
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry, try again");
+		console.log("Sorry, try again");
 	}
 }
+
+var flipCard = function(cardID) {
+
+console.log("User flipped " + cards[cardID]);
+
+cardsInPlay.push(cards[cardID]);
+
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
